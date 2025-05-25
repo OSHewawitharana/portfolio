063c711d02c67760c66a22ab25f4d59927cb9dd2
OSHewawitharana <oshewawitharana123@gmail.com>
2025-05-25 11:10:57 +1000

    Alt image error fix in build

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -162,7 +162,7 @@ export const Skills = () => {
                     </div>
                 </div>
             </div>
-            <img className="background-image-left" src={colorSharp} alt="Image" />
+            <img className="background-image-left" src={colorSharp} alt="Background" />
         </section>
     );
 }
