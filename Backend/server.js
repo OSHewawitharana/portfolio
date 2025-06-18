2f04eb1919270e01fe9a239fdbfc77bb4a68b6f6
OSHewawitharana <oshewawitharana123@gmail.com>
2025-06-18 13:07:33 +1000

    username change

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
